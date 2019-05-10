845b3af37d3b74c0732be5ccfddfdb1fa90e776b
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-10 18:20:02 +0100

    add tests

---
+++ test/mapCommentsTest.js
@@ -0,0 +1,64 @@
+const expect =require('expect.js');
+const fs = require('fs');
+
+const mapComments = require('../src/index').mapComments;
+
+describe('MapComments', () => {
+    const filePath = 'test/contracts/ERC20.sol';
+    let comments = '';
+
+    before(() => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        comments = mapComments(input);
+    });
+
+    describe('extract @dev comments', () => {
+        it('extract single line no param/return valid comments', () => {
+            // verify
+            expect(comments.function.get('totalSupply').dev.trim()).to.be('Total number of tokens in existence');
+        });
+        it('extract single line w/ param/return valid comments', () => {
+            // verify
+            expect(comments.function.get('balanceOf').dev.trim()).to.be('Gets the balance of the specified address.');
+        });
+        it('extract multiline line w/ param/return valid comments', () => {
+            // verify
+            expect(comments.function.get('transferFrom').dev.trim()).to.be('Transfer tokens from one address to another. '
+                + 'Note that while this function emits an Approval event, this is not required as per the '
+                + 'specification, and other compliant implementations may not emit the event.');
+        });
+        it('extract multiline line w/ code in valid comments', () => {
+            // verify
+            expect(comments.function.get('decreaseAllowance').dev.trim()).to.be('Decrease the amount of tokens that an '
+                + 'owner allowed to a spender. approve should be called when '
+                + '_allowed[msg.sender][spender] == 0. To decrement allowed value is better '
+                + 'to use this function to avoid 2 calls (and wait until the first transaction is '
+                + 'mined) From MonolithDAO Token.sol Emits an Approval event.');
+        });
+        it('extract multiline line w/ link in valid comments', () => {
+            // verify
+            expect(comments.function.get('approve').dev.trim()).to.be('Approve the passed address to spend the '
+                + 'specified amount of tokens on behalf of msg.sender. Beware that changing an '
+                + 'allowance with this method brings the risk that someone may use both the old '
+                + 'and the new allowance by unfortunate transaction ordering. One possible '
+                + "solution to mitigate this race condition is to first reduce the spender's "
+                + 'allowance to 0 and set the desired value afterwards: https://github.com'
+                + '/ethereum/EIPs/issues/20#issuecomment-263524729');
+        });
+        it('extract single line w/ _ in function name valid comments', () => {
+            // verify
+            expect(comments.function.get('_transfer').dev.trim()).to.be('Transfer token for a specified addresses');
+        });
+    });
+    describe('extract @param comments', () => {
+        it('extract two params w/ valid comments', () => {
+            // verify
+            expect(comments.function.get('allowance').param.get('owner').trim())
+                .to.be('The address which owns the funds.');
+            expect(comments.function.get('allowance').param.get('spender').trim())
+                .to.be('The address which will spend the funds.');
+        });
+    });
+});
