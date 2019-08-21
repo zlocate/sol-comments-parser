9f5d414d719d127078877202ba506d23aefb84df
Denis <43764701+zlocate@users.noreply.github.com>
2019-08-21 10:13:16 +0000

    Update tests

--- test/contracts/Single.sol
+++ test/contracts/Single.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.5.6;
  * @notice hello!
  */
 contract Single {
+    //TODO: is comment without any notation and spaces (needs ignore)
+    // this is comment without any notation (needs ignore)
     // @dev this is a single line comment
     function line() public pure returns(uint256) {
-        return 0;
+        return 0; // this is inline comment without notation (needs ignore)
     }
 
     /// @dev this is a single corner comment
     function corner() public pure returns(uint256) {
-        return 0;
+        return 0;  // @dev this is inline comment with notation
     }
 }
